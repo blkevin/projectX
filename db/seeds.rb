c43e02c472ba80ce9ee676f41a36eb45299a80ee
Gabriel de Monts <gabriel.demonts@gmail.com>
2022-11-29 18:15:28 +0100

    seeding users, institutions, educations and experiences

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+require 'open-uri'
+
 puts "Deleting students"
 students = User.student
 students.destroy_all
@@ -20,7 +22,34 @@ s6 = User.create!(first_name: "Jean", last_name: "Dupont", age: 18, email: "jean
 s7 = User.create!(first_name: "Nicolas", last_name: "Bernard", age: 19, email: "nicolas@gmail.com", password: "123456", description: "J'aimerais tout quitter pour me lancer dans le rap", status: :student)
 s8 = User.create!(first_name: "Julie", last_name: "Martin", age: 16, email: "julie@gmail.com", password: "123456", description: "J'aimerais faire une carrière au service de l'Etat", status: :student)
 s9 = User.create!(first_name: "Madeleine", last_name: "Robert", age: 17, email: "madeleine@gmail.com", password: "123456", description: "Intéréssée par l'artisanat", status: :student)
-s10 = User.create!(first_name: "Jean", last_name: "Durand", age: 18, email: "jean@gmail.com", password: "123456", description: "Etudiant intéréssé par le cinema", status: :student)
+s10 = User.create!(first_name: "Baptiste", last_name: "Durand", age: 18, email: "baptiste@gmail.com", password: "123456", description: "Etudiant intéréssé par le cinema", status: :student)
+
+# attaching pictures to students
+s1.photo.attach(
+  io: URI.open('https://avatars.githubusercontent.com/u/113995804\?v\=4.jpg'),
+  filename: 'gabriel.jpg',
+  content_type: 'image/jpg'
+)
+s2.photo.attach(
+  io: URI.open('https://avatars.githubusercontent.com/u/112618478\?v\=4.jpg'),
+  filename: 'robin.jpg',
+  content_type: 'image/jpg'
+)
+s3.photo.attach(
+  io: URI.open('https://avatars.githubusercontent.com/u/62299446\?v\=4.jpg'),
+  filename: 'sous-chef.jpg',
+  content_type: 'image/jpg'
+)
+s4.photo.attach(
+  io: URI.open('https://avatars.githubusercontent.com/u/115085959\?v\=4.jpg'),
+  filename: 'mehdi.jpg',
+  content_type: 'image/jpg'
+)
+s4.photo.attach(
+  io: URI.open('https://avatars.githubusercontent.com/u/115102708\?v\=4.jpg'),
+  filename: 'chef.jpg',
+  content_type: 'image/jpg'
+)
 
 puts "Deleting professionals"
 mentors = User.mentor
@@ -28,7 +57,7 @@ mentors.destroy_all
 
 puts "Creating professionals"
 m1 = User.create!(first_name: "Paul", last_name: "Portier", age: 30, email: "paul@gmail.com", password: "123456", description: "Meilleur dev en région parisienne", status: :mentor)
-m2 = User.create!(first_name: "Monica", last_name: "Bellucci", age: 58, email: "monica@gmail.com", password: "123456", description: "Actrice sans prétention", status: :mentor)
+m2 = User.create!(first_name: "Carla", last_name: "Bruni", age: 58, email: "carla@gmail.com", password: "123456", description: "Actrice/chanteuse sans prétention", status: :mentor)
 m3 = User.create!(first_name: "Théobald", last_name: "de Bentzmann", age: 32, email: "theobald@gmail.com", password: "123456", description: "Born leader - un chef c'est fait pour cheffer", status: :mentor)
 m4 = User.create!(first_name: "Eric", last_name: "Dupont-Moretti", age: 61, email: "erci@gmail.com", password: "123456", description: "Avocat et garde des Sceaux", status: :mentor)
 m5 = User.create!(first_name: "Jonathan", last_name: "Cohen", age: 42, email: "jonathan@gmail.com", password: "123456", description: "Homme le plus drole de France", status: :mentor)
@@ -38,5 +67,76 @@ puts "Deleting institutions"
 Institution.destroy_all
 
 puts "Creating institutions"
-i1 = Institution.create!(name: "ESCP Business School", description: "Let's empower and lead the world in the right direction")
-i2 = Institution.create!(name: "Le Wagon", description: "Accélérez votre carrière, formez-vous aux métiers de la tech")
+i1 = Institution.create!(name: "Le Wagon", description: "Accélérez votre carrière, formez-vous aux métiers de la tech")
+i2 = Institution.create!(name: "Conservatoire nationale supérieur d'art dramatique", description: "Le Conservatoire National Supérieur d’Art Dramatique – PSL (CNSAD-PSL) est un établissement d’enseignement supérieur placé sous la tutelle du ministère chargé de la culture")
+i3 = Institution.create!(name: "ESCP Business School", description: "Let's empower and lead the world in the right direction")
+i4 = Institution.create!(name: "Université Lille 2", description: "Inspirons demain")
+i5 = Institution.create!(name: "Université Paris-Panthéon-Assas", description: "Pour l'éducation, pour la nation")
+i6 = Institution.create!(name: "Les compagnons du devoir", description: "Association ouvrière des compagnons du devoir et du tour de France")
+
+puts "Deleting educations"
+Education.destroy_all
+
+puts "Creating educations"
+ed1 = Education.create!(start_date: "2016-10-20", degree_level: "Bootcamp", field: "Développement Web")
+ed2 = Education.create!(start_date: "1990-09-01", degree_level: "License", field: "Theatre & cinema")
+ed3 = Education.create!(start_date: "2000-09-01", degree_level: "Master", field: "Finance")
+ed4 = Education.create!(start_date: "2000-09-01", degree_level: "Master", field: "Droit")
+ed5 = Education.create!(start_date: "1979-09-01", degree_level: "Master", field: "Theatre")
+ed6 = Education.create!(start_date: "1979-09-01", degree_level: "License", field: "Histoire")
+
+puts "Deleting experiences"
+Experience.destroy_all
+
+puts "Creating experiences"
+# creating experiences for Paul Portier
+exp1 = Experience.create!(position: "Developpeur web", sector: "luxe", industry: "digital", start_date: "2016-01-01")
+exp2 = Experience.create!(position: "Barman", sector: "restauration", industry: "autre activité de services", start_date: "2014-01-01")
+# creating experiences for Carla Bruni
+exp3 = Experience.create!(position: "Acteur", sector: "cinema", industry: "artisanat", start_date: "1995-01-01")
+exp4 = Experience.create!(position: "Chanteur", sector: "musique", industry: "artisanat", start_date: "2000-01-01")
+# creating experiences for Théobald de Bentzmann
+exp5 = Experience.create!(position: "PDG", sector: "evenementiel", industry: "service B2B", start_date: "2017-01-01")
+exp6 = Experience.create!(position: "Analyste fianancier", sector: "finance", industry: "services financiers", start_date: "2013-01-01")
+# creating experiences for Eric Dupont-Moretti
+exp7 = Experience.create!(position: "Ministre de la Justice", sector: "publique", industry: "administration publique", start_date: "2020-01-01")
+exp8 = Experience.create!(position: "Avocat", sector: "juridique", industry: "activité juridique", start_date: "1984-01-01")
+# creating experiences for Jonathan Cohen
+exp9 = Experience.create!(position: "Acteur", sector: "cinema", industry: "artisanat", start_date: "1995-01-01")
+exp10 = Experience.create!(position: "Commercial", sector: "immobilier", industry: "activité immobilière", start_date: "2000-01-01")
+# creating experiences for Kaaris
+exp11 = Experience.create!(position: "Acteur", sector: "cinema", industry: "artisanat", start_date: "2016-01-01")
+exp12 = Experience.create!(position: "Chanteur", sector: "musique", industry: "artisanat", start_date: "2013-01-01")
+
+# matching experiences with mentors
+exp1.user_id = m1.id
+exp2.user_id = m1.id
+exp3.user_id = m2.id
+exp4.user_id = m2.id
+exp5.user_id = m3.id
+exp6.user_id = m3.id
+exp7.user_id = m4.id
+exp8.user_id = m4.id
+exp9.user_id = m5.id
+exp10.user_id = m5.id
+exp11.user_id = m6.id
+exp12.user_id = m6.id
+
+# matching educations with institutions and mentors
+ed1.institution_id = i1.id
+ed1.user_id = m1.id
+
+ed2.institution_id = i2.id
+ed2.user_id = m2.id
+
+ed3.institution_id = i3.id
+ed3.user_id = m3.id
+
+ed4.institution_id = i4.id
+ed4.user_id = m4.id
+
+ed5.institution_id = i2.id
+ed5.user_id = m5.id
+
+ed6.institution_id = i5.id
+ed6.user_id = m6.id
