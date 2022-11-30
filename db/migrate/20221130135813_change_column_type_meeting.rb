06472bb1e2de0355bdeb79ee78d69e71f8f9d8e8
robin rettien <robinrettien@gmail.com>
2022-11-30 16:41:08 +0100

    managed to make simple form

--- db/migrate/20221130135813_change_column_type_meeting.rb
+++ db/migrate/20221130135813_change_column_type_meeting.rb
@@ -1,6 +1,8 @@
 class ChangeColumnTypeMeeting < ActiveRecord::Migration[7.0]
   def change
-    change_column :meetings, :starting, :datetime, null: false
-    change_column :meetings, :ending, :datetime, null: false
+    remove_column :meetings, :starting
+    remove_column :meetings, :ending
+    add_column :meetings, :starting, :datetime, null: false
+    add_column :meetings, :ending, :datetime, null: false
   end
 end
