6ec88bfe6fbf4353b6e4f1190ba5b874af30f72d
Gabriel de Monts <gabriel.demonts@gmail.com>
2022-12-02 16:07:38 +0100

    show institution

--- app/controllers/institutions_controller.rb
+++ app/controllers/institutions_controller.rb
@@ -1,7 +1,10 @@
 class InstitutionsController < ApplicationController
   def show
     @institution = Institution.find(params[:id])
-    @education = Education.find_by(institution_id: @institution.id)
-    @mentor = User.mentor.find(@education.user_id)
+    @mentors = @institution.users
+
+    @educations = Education.all.where(institution_id: @institution.id)
+    # find all user.mentor which belong to institution
+    # trouver XP depuis instituion
   end
 end
