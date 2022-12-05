2606b1e792954de1f4350fb2061d53280a174c8d
robin rettien <robinrettien@gmail.com>
2022-12-05 11:42:52 +0100

    first changes

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -10,5 +10,8 @@ application.register("conversation-subscription", ConversationSubscriptionContro
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
-import NavbarController from "./navbar_controller.js"
+import NavbarController from "./navbar_controller"
 application.register("navbar", NavbarController)
+
+import ScheduleMeetingController from "./schedule_meeting_controller"
+application.register("schedule-meeting", ScheduleMeetingController)
---
+++ app/javascript/controllers/schedule_meeting_controller.js
@@ -0,0 +1,7 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="schedule-meeting"
+export default class extends Controller {
+  connect() {
+  }
+}
