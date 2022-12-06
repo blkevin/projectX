afab427539a0a1233a62ec281d6c59b29292ae3d
Kevin Blakime <blakime.kevin@gmail.com>
2022-12-06 18:09:31 +0100

    stop redirecting en boucle

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :redirect_if_incomplete_user, unless: :devise_controller?
+  # before_action :redirect_if_incomplete_user, unless: :devise_controller?
 
   protected
 
