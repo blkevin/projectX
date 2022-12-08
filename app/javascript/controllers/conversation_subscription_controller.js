6f50590012c05286070f77055d8b6530059127ab
robin rettien <robinrettien@gmail.com>
2022-12-08 17:00:17 +0100

    all final changes

--- app/javascript/controllers/conversation_subscription_controller.js
+++ app/javascript/controllers/conversation_subscription_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 import { createConsumer } from "@rails/actioncable"
 
 export default class extends Controller {
-  static values = { conversationId: Number }
+  static values = { conversationId: Number, currentUserId: Number }
   static targets = ["messages"]
 
   connect() {
@@ -23,9 +23,43 @@ export default class extends Controller {
     this.channel.unsubscribe()
   }
 
+  #buildMessageElement(currentUserIsSender, message) {
+    return `
+      <div class="message-row d-flex ${this.#justifyClass(currentUserIsSender)}">
+        <div class="${this.#userStyleClass(currentUserIsSender)}">
+          ${message}
+        </div>
+      </div>
+    `
+  }
+
   #insertMessageAndScrollDown(data) {
-    this.messagesTarget.insertAdjacentHTML("beforeend", data)
+    const currentUserIsSender = this.currentUserIdValue === data.sender_id
+    console.log(currentUserIsSender);
+    console.log(data);
+    console.log(data.message);
+    if (data.message) {
+      const message = data.message
+      const messageElement = this.#buildMessageElement(currentUserIsSender, message)
+      this.messagesTarget.insertAdjacentHTML("beforeend", messageElement)
+    }
+    else {
+      const messageElement = data
+      console.log(messageElement);
+      this.messagesTarget.insertAdjacentHTML("beforeend", messageElement)
+    }
+
+
     this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
   }
+
+  #justifyClass(currentUserIsSender) {
+    return currentUserIsSender ? "justify-content-end" : "justify-content-start"
+  }
+
+  #userStyleClass(currentUserIsSender) {
+    return currentUserIsSender ? "sender-style" : "receiver-style"
+  }
+
 
 }
--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,9 @@ class Message < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :meeting, optional: true
   validates :content, presence: true
+
+  def sender?(a_user)
+    user.id == a_user.id
+  end
+
 end
